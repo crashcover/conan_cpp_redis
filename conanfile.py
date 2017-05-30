f9312976c37237c0da33ff843160b449013b72dc
Andreas Daeubler <ad@robominds.de>
2017-05-30 14:07:24 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake
+
+
+class CppredisConan(ConanFile):
+    name = "cpp_redis"
+    description = "C++11 Lightweight Redis client: async, thread-safe, no dependency, pipelining, multi-platform."
+    version = "3.5.1"
+    license = "MIT License"
+    url = "https://github.com"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    generators = "cmake"
+    folder_name = "cpp_redis"
+
+    def source(self):
+        self.run("git clone --recursive https://github.com/Cylix/cpp_redis.git")
+        self.run("cd cpp_redis && git checkout tags/%s" % self.version)
+
+    def build(self):
+        cmake = CMake(self)
+        shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
+        self.run('cmake %s %s %s' % (self.name, cmake.command_line, shared))
+        self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*", dst="include", src=self.folder_name+"/includes")
+        self.copy("*", dst="include", src=self.folder_name+"/tacopie/includes")
+        self.copy("*.lib", dst="lib", src="lib")
+        self.copy("*.so", dst="lib", src="lib")
+        self.copy("*.a", dst="lib", src="lib")
+
+    def package_info(self):
+        self.cpp_info.libs = ["cpp_redis", "tacopie"]
---
+++ test_package/example.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cpp_redis/cpp_redis>
+
+int main() {
+	cpp_redis::redis_client client;
+}
